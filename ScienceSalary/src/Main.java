907cba2f80bbafb464f63368363ae07e04d8ef65
Михаил Горбань <91434795+usernameisavailablee@users.noreply.github.com>
2023-09-21 23:00:54 +0300

    123

--- ScienceSalary/src/Main.java
+++ ScienceSalary/src/Main.java
@@ -1,33 +1,24 @@
 import Classes.Engeneer;
+import Classes.JuniorResearcher;
 import Classes.Scientist;
+import Classes.SeniorResearcher;
 
 public class Main {
     public static void main(String[] args) {
 
         Scientist[] arr = new Scientist[9];
 
-        Scientist person1 = new Engeneer(12,120000);
-        Scientist person2 = new Engeneer(12,120000);
-        Scientist person3 = new Engeneer(12,120000);
-        Scientist person4 = new Engeneer(12,120000);
-        Scientist person5 = new Engeneer(2,140000);
-        Scientist person6 = new Engeneer(3,140000);
-        Scientist person7 = new Engeneer(17,150000);
-        Scientist person8 = new Engeneer(14,150000);
-
-        arr[0] = person1;
-        arr[1] = person2;
-        arr[2] = person3;
-        arr[3] = person4;
-        arr[4] = person5;
-        arr[5] = person6;
-        arr[6] = person7;
-        arr[7] = person8;
+        arr[0] = new Engeneer(12,120000);
+        arr[1] = new Engeneer(12,120000);
+        arr[2] = new Engeneer(12,120000);
+        arr[3] = new Engeneer(12,120000);
+        arr[4] = new JuniorResearcher(2,140000);
+        arr[5] = new JuniorResearcher(3,140000);
+        arr[6] = new SeniorResearcher(17,150000);
+        arr[7] = new SeniorResearcher(14,150000);
         
-
-
-        System.out.print(person1.toString());
-        System.out.print(person1.allowance());
+        System.out.print(arr[0].toString());
+        System.out.print(arr[0].allowance());
 
 
 
